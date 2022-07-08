8901af561d4141f7064a16e358ceb7e591593919
2450123 <2450144961@qq.com>
2022-07-08 19:38:48 +0800

    注册功能邮件部分（未完成）

--- src/main/java/com/example/jkm_web/util/EmailUtil.java
+++ src/main/java/com/example/jkm_web/util/EmailUtil.java
@@ -1,27 +1,32 @@
 package com.example.jkm_web.util;
 
 import com.example.jkm_web.model.Teacher;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Properties;
 
 @Component
 public class EmailUtil {
+
+    private final static Logger logger = LoggerFactory.getLogger(EmailUtil.class);
+
     //读取html模板
-    private static String readHtmlToString(String htmlPath) {
+    public static String readHtmlToString(String htmlPath) {
         InputStream is = null;
         Reader reader = null;
         try {
-            is = EmailUtil.class.getClassLoader().getResourceAsStream(htmlPath);
+            is = new ClassPathResource(htmlPath).getInputStream();
             if (is == null) {
                 throw new Exception("未找到html模板文件");
             }
@@ -59,14 +64,38 @@ public class EmailUtil {
     private JavaMailSender javaMailSender;
     @Value("${spring.mail.username}")
     private String from;
+    @Value("${ip}")
+    private String ip;
 
     public String sendEmailActivation(Teacher teacher) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom(from);
-        simpleMailMessage.setTo(teacher.getEmail());
-        simpleMailMessage.setSubject("Test");
-        simpleMailMessage.setText("Test");
-        javaMailSender.send(simpleMailMessage);
+        try {
+            //读取模板文件
+            String filePath = "static/sendEmailActivationHtml.html";
+            String html = readHtmlToString(filePath);
+
+            //写入参数
+            Document document = Jsoup.parse(html);
+            document.getElementById("userId").html(teacher.getName());
+            //document.getElementById("servletIP").attr("href", "http://" + ip + "/rxyl/emailActivationCode.do?code=" + emailActivation + "&email=" + user.getEmail() + "&sessionId=" + sessionId);
+
+            MimeMessageHelper messageHelper = new MimeMessageHelper(javaMailSender.createMimeMessage(), true);
+            messageHelper.setFrom(from);
+            messageHelper.setTo(teacher.getEmail());
+            messageHelper.setSubject("激活账号");
+            messageHelper.setText(document.toString(),true);
+
+            javaMailSender.send(messageHelper.getMimeMessage());
+            logger.info("发送邮件成功:" + teacher.getEmail());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//        simpleMailMessage.setFrom(from);
+//        simpleMailMessage.setTo(teacher.getEmail());
+//        simpleMailMessage.setSubject("Test");
+//        simpleMailMessage.setText("Test");
+//        javaMailSender.send(simpleMailMessage);
         return "";
     }
 }
